c302f92ad96b150bdd7110bdf87ec6ec8d1e3735
ConxiCr <conxicr@hotmail.com>
2021-03-04 14:18:30 +0100

    firts commit
    Signed-off-by: ConxiCr <conxicr@hotmail.com>

---
+++ main.js
@@ -0,0 +1,10 @@
+//TODO LIST
+
+// 1. DEFINIR. Qué queremos??
+// Necesitamos una web con un input y un botón, que eal introducir una tarea, se añada a una lista
+// Proceso: usuario introduce valor -> pulsa botón->añadimos elemento a la lista
+
+// 2. TAREAS
+// ->COMPLETAR HTML(commit/git)
+// MONTAR BASE JS
+//RELLENA FUNCIONES

ba301dcc3234c2ee81ec3ed9ac71e0229a908e01
ConxiCr <conxicr@hotmail.com>
2021-03-05 10:04:31 +0100

    create variables
    Signed-off-by: ConxiCr <conxicr@hotmail.com>

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 //TODO LIST
 
 // 1. DEFINIR. Qué queremos??
-// Necesitamos una web con un input y un botón, que eal introducir una tarea, se añada a una lista
+// Necesitamos una web con un input y un botón, que al introducir una tarea, se añada a una lista
 // Proceso: usuario introduce valor -> pulsa botón->añadimos elemento a la lista
 
 // 2. TAREAS
 // ->COMPLETAR HTML(commit/git)
 // -MONTAR BASE JS
 // -RELLENA FUNCIONES
+
+
+//VARIABLES
+//Con qué elementos del HTML voy a interaccionar?
+//Botón
+//Input
+//Listado para enganchar items -> ul
+
+var enterButton = document.getElementById('enter');
+var input = document.getElementById("userInput");
+var ul =document.querySelector("ul");
+
+//FUNCTIONS
+
+
+//EVENTS
